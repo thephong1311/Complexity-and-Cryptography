bf08a2154c2a0482ee6c8cd386890700209aa403
Nguyễn Thế Phong <105076865+thephong1311@users.noreply.github.com>
2024-12-26 11:22:02 +0700

    Add files via upload

---
+++ Block.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VdBlockchain.Classes;
+
+namespace VdBlockchain
+{
+    class Block
+    {
+        public String hash;
+        public String previousHash;
+        private String data;
+
+        private long timeStamp;
+        private int nonce = 0;
+
+        public Block(String data, String previousHash)
+        {
+            this.data = data;
+            this.previousHash = previousHash;
+            this.timeStamp = DateTimeHandle.GetTime();
+            this.hash = CalculateHash();
+        }
+
+        public String CalculateHash()
+        {
+            Sha256 sha256 = new Sha256();
+            String calculatedhash = sha256.Hash(previousHash + timeStamp.ToString() + nonce.ToString() + data);
+            return calculatedhash;
+        }
+    }
+}
---
+++ program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VdBlockchain.Classes;
+
+namespace VdBlockchain
+{
+    class Program
+    {
+        public static List<Block> blockchain = new List<Block>();
+        static void Main(string[] args)
+        {
+            blockchain.Add(new Block("Nguyen The Phong", "0"));
+            blockchain.Add(new Block("Mat ma va do phuc tap thuat toan", blockchain.ElementAt(blockchain.Count - 1).hash));
+            blockchain.Add(new Block("Khoa Toan Tin", blockchain.ElementAt(blockchain.Count - 1).hash));
+            blockchain.Add(new Block("Dai hoc Bach khoa Ha Noi", blockchain.ElementAt(blockchain.Count - 1).hash));
+
+            Console.WriteLine("Blockchain is valid: " + IsChainValid());
+
+            string printBlockChain = new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(blockchain);
+            Console.WriteLine(printBlockChain);
+
+            string data, previousHash;
+            Console.WriteLine();
+            Console.WriteLine("New Block");
+            Console.Write("Data: ");
+            data = Console.ReadLine();
+            Console.Write("Previous Hash: ");
+            previousHash = Console.ReadLine();
+            blockchain.Add(new Block(data, previousHash));
+            string printBlockChain1 = new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(blockchain);
+            Console.WriteLine(printBlockChain1);
+            Console.WriteLine("Blockchain is valid: " + IsChainValid());
+            Console.ReadLine();
+        }
+
+        public static Boolean IsChainValid()
+        {
+            Block currentBlock;
+            Block previousBlock;
+
+            for (int i = 1; i < blockchain.Count; i++)
+            {
+                currentBlock = blockchain.ElementAt(i);
+                previousBlock = blockchain.ElementAt(i - 1);
+
+                if (!currentBlock.hash.Equals(currentBlock.CalculateHash()))
+                {
+                    Console.WriteLine("Current Hashes not equal");
+                    return false;
+                }
+
+                if (!previousBlock.hash.Equals(currentBlock.previousHash))
+                {
+                    Console.WriteLine("Previous Hashes not equal");
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
